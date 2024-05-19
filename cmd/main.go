a7a948c46c0324f6d61bf3591785a63c1dce6d91
skantay <skantay03@gmail.com>
2024-05-19 23:51:24 +0500

    uploading bot layer

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,32 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+	"transcripter_bot/internal/bot"
+
+	"github.com/PaulSonOfLars/gotgbot/v2"
+)
 
 func main() {
 	fmt.Println("Starting new project")
+
+	token := os.Getenv("TELEGRAM_BOT_TOKEN")
+
+	botClient, err := gotgbot.NewBot(token, nil)
+	if err != nil {
+		log.Printf("failed to connect to bot: %v", err)
+
+		return
+	}
+
+	// TODO: implement transcriberService, searchService
+	botController := bot.NewBotController(nil, nil)
+
+	if err := bot.RunTelegramBot(botClient, botController); err != nil {
+		log.Printf("failed to run the project: %v", err)
+
+		return
+	}
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module transcripter_bot
 
 go 1.22
+
+require github.com/PaulSonOfLars/gotgbot/v2 v2.0.0-rc.27
