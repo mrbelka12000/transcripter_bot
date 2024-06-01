5760f3b8886b039e8c88f8224a24ed78bbb0bfe7
skantay <skantay03@gmail.com>
2024-06-01 18:56:02 +0500

    making assembly struct exported

--- internal/client/assembly/assembly.go
+++ internal/client/assembly/assembly.go
@@ -6,17 +6,17 @@ import (
 	"github.com/AssemblyAI/assemblyai-go-sdk"
 )
 
-type assembly struct {
+type Assembly struct {
 	client *assemblyai.Client
 }
 
-func NewAssembly(apiKey string) *assembly {
-	return &assembly{
+func NewAssembly(apiKey string) *Assembly {
+	return &Assembly{
 		client: assemblyai.NewClient(apiKey),
 	}
 }
 
-func (s *assembly) TranscribeAudio(ctx context.Context, url string) (string, error) {
+func (s *Assembly) TranscribeAudio(ctx context.Context, url string) (string, error) {
 	params := assemblyai.TranscriptOptionalParams{
 		LanguageCode: "ru",
 	}
